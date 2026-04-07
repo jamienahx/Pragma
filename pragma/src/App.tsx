dea51f5c7b37597ffd184439f761bd3be10ed0cc
jamienahx <jamie.nahx@gmail.com>
2026-04-07 20:58:31 +0800

    linked navbar in app

--- pragma/src/App.tsx
+++ pragma/src/App.tsx
@@ -3,15 +3,21 @@ import {Routes, Route} from 'react-router-dom';
 import './App.css';
 import Form from './components/form';
 import Results from './components/results';
+import Navbar from './components/navbar';
 
 function App() {
   return(
+    <>
+
+     <Navbar />
     <Routes>
+     
 
     <Route path="/" element = {<Form />} />
     <Route path="/results" element = {<Results/>} />
 
     </Routes>
+    </>
 
   )
 }
